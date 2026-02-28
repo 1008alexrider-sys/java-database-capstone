414b9ac9b260929c15566dbea796e1ba73f88957
satyaprakashpatel024 <satyaprakashpatel61@gmail.com>
2026-03-01 02:35:52 +0530

    Refactor model classes to remove boilerplate code and update .gitignore for resource management

--- app/src/main/java/com/project/back_end/BackEndApplication.java
+++ app/src/main/java/com/project/back_end/BackEndApplication.java
@@ -1,34 +1,13 @@
 package com.project.back_end;
 
-import com.project.back_end.repo.PrescriptionRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 @ComponentScan("com.project.back_end")
 public class BackEndApplication {
-
-	private final PrescriptionRepository prescriptionRepository;
-
-	public BackEndApplication(PrescriptionRepository prescriptionRepository) {
-		this.prescriptionRepository = prescriptionRepository;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String index() {
-		return "Welcome to the Prescription Management System API!";
-	}
-
-	@GetMapping("/health")
-	public String healthCheck() {
-		return "API is up and running!";
 	}
 }
